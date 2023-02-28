13f3b55b714e8c9bd869d0596a58b8ecf5500df5
Maciej Procyk <macieekprocyk@gmail.com>
2023-02-28 09:55:06 +0100

    add options to run script

--- start.sh
+++ start.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 
+help()
+{
+  echo "Run docker services script for mini-games environment"
+  echo "Available options:"
+  echo "  -d - run only database service"
+  echo "  -h - display this help page"
+}
+
+while getopts "dh" opt      # get options for -a and -b ( ':' - option has an argument )
+do
+    case $opt in
+        d) docker compose --file docker-compose.yml --env-file .env build mini-games-dev-postgres
+           docker compose --file docker-compose.yml --env-file .env up mini-games-dev-postgres
+           exit;;
+        h) help
+           exit;;
+        *) help
+           exit;;
+    esac
+done
+
 docker compose --file docker-compose.yml --env-file .env build
 docker compose --file docker-compose.yml --env-file .env up
