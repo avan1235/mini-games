cab6e23f7c4611138dbd69a293d97f51dac9f386
avan1235 <macieekprocyk@gmail.com>
2023-04-05 22:43:16 +0200

    migrate to newer compose and kotlin vesion

--- build-src/src/main/kotlin/Constants.kt
+++ build-src/src/main/kotlin/Constants.kt
@@ -2,7 +2,7 @@ object Constants {
 
     object Android {
         const val minSdk = 21
-        const val compileSdk = 32
-        const val targetSdk = 32
+        const val compileSdk = 33
+        const val targetSdk = 33
     }
 }
--- shared-client/build.gradle.kts
+++ shared-client/build.gradle.kts
@@ -13,7 +13,9 @@ plugins {
 
 kotlin {
     android()
+
     jvm("desktop")
+
     sourceSets {
         named("commonMain") {
             dependencies {
