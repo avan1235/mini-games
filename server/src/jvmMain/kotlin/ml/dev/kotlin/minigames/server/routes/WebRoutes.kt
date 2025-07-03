601e689754147d25bc71207db279f955eb0dae99
Maciej Procyk <macieekprocyk@gmail.com>
2025-07-03 22:26:53 +0200

    select db version before each status check on main site

--- server/src/jvmMain/kotlin/ml/dev/kotlin/minigames/server/routes/WebRoutes.kt
+++ server/src/jvmMain/kotlin/ml/dev/kotlin/minigames/server/routes/WebRoutes.kt
@@ -4,13 +4,20 @@ import io.ktor.server.application.*
 import io.ktor.server.html.*
 import io.ktor.server.routing.*
 import kotlinx.html.*
+import ml.dev.kotlin.minigames.db.suspendedTxn
 import ml.dev.kotlin.minigames.shared.api.MAIN_SITE
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 fun Application.webRoutes() = routing {
     get(MAIN_SITE) { respondMainSite() }
 }
 
 private suspend fun RoutingContext.respondMainSite() {
+    suspendedTxn {
+        val conn = TransactionManager.current().connection
+        val statement = conn.prepareStatement("SELECT version();", false)
+        statement.executeQuery()
+    }
     call.respondHtml {
         head {
             title { +"Mini Games" }
